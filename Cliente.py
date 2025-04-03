b3e4ba755318c3dfc3c3d156dd7fec4f8690c04e
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2025-04-03 18:43:34 -0300

    alterei conforme o monitor pediu

--- Cliente.py
+++ Cliente.py
@@ -1,12 +1,28 @@
 import socket
 
+def solicitarTamanho():
+    while True:
+        mensagem = input("Informe o tamanho da mensagem (tem que ter no maximo 3 caracteres): ")
+        if len(mensagem)<=3:
+            return mensagem
+        else:
+            print("Tente novamente")
+
+def modoOperacao():
+    while True:
+        modo = input("Informe o modo de operação (go-back-n ou repeticao seletiva): ").lower()
+        if modo in ["go-back-n", "repeticao seletiva"]:
+            return modo
+        else:
+            print("Tente novamente")
+            
 soquete_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 endereco_servidor = ("localhost", 1024)
 soquete_cliente.connect(endereco_servidor)
 
-modo_operacao = "go-back-n"
-tamanho_mensagem = 3
+modo_operacao = modoOperacao()
+tamanho_mensagem = solicitarTamanho()
 
 dados_para_servidor = f"{modo_operacao},{tamanho_mensagem}"
 soquete_cliente.sendall(dados_para_servidor.encode())
